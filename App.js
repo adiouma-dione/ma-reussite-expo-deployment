bcd2f2faba8b610cc16258486f63ca66b08bd43c
adiouma-dione <adiouma10@gmail.com>
2024-07-19 21:06:57 +0000

    [FIX] - combine App.js and PrivacyPolicyScreen.js files to avoid display bugs

--- App.js
+++ App.js
@@ -1,43 +1,69 @@
 import React, { useEffect, useState } from "react";
-import { NativeBaseProvider } from "native-base";
+import { Button, NativeBaseProvider } from "native-base";
+import { WebView } from "react-native-webview";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as SplashScreen from "expo-splash-screen";
 import useFonts from "./src/hooks/useFonts";
 import AppNavigator from "./src/navigation/AppNavigator";
 import customTheme from "./src/themes/customTheme";
-import PrivacyPolicyScreen from "./src/screens/PrivacyPolicyScreen";
 
 SplashScreen.preventAutoHideAsync();
 
 const App = () => {
   const [policyAccepted, setPolicyAccepted] = useState(false);
+  const [currentPolicyVersion, setCurrentPolicyVersion] = useState(null);
+
+  const handleWebViewMessage = (event) => {
+    const policyVersion = event.nativeEvent.data;
+    setCurrentPolicyVersion(policyVersion);
+  };
+
+  const PrivacyPolicyScreen = ({ onAccept }) => {
+    return (
+      <NativeBaseProvider isSSR theme={customTheme}>
+        <WebView
+          source={{ uri: "https://ma-reussite-privacy.netlify.app/" }}
+          style={{ flex: 1, marginTop: "10%", marginBottom: "5%" }}
+          onMessage={handleWebViewMessage}
+        />
+        <Button w={"50%"} mx={"auto"} mb={"2%"} onPress={onAccept}>
+          Accept
+        </Button>
+      </NativeBaseProvider>
+    );
+  };
 
   const fontsLoaded = useFonts({
     "Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
     "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
   });
 
-  useEffect(() => {
-    const checkPolicyAcceptance = async () => {
-      const value = await AsyncStorage.getItem("policyAccepted");
-      if (value === "true") {
-        setPolicyAccepted(true);
-      }
-    };
+  const handleAccept = async () => {
+    await AsyncStorage.setItem("policyAccepted", "true");
+    await AsyncStorage.setItem("policyVersion", currentPolicyVersion);
+    setPolicyAccepted(true);
+  };
 
-    checkPolicyAcceptance();
-  }, []);
+  const checkPolicyAcceptance = async () => {
+    const value = await AsyncStorage.getItem("policyAccepted");
+    const storedPolicyVersion = await AsyncStorage.getItem("policyVersion");
+
+    if (value === "true" && storedPolicyVersion === currentPolicyVersion) {
+      setPolicyAccepted(true);
+    }
+  };
 
   useEffect(() => {
-    if (fontsLoaded) SplashScreen.hideAsync();
-  }, [fontsLoaded]);
+    if (currentPolicyVersion) checkPolicyAcceptance();
+    if (fontsLoaded && currentPolicyVersion) SplashScreen.hideAsync();
+  }, [fontsLoaded, currentPolicyVersion]);
 
   if (!fontsLoaded) {
     return null;
   }
 
   if (!policyAccepted) {
-    return <PrivacyPolicyScreen setPolicyAccepted={setPolicyAccepted} />;
+    return <PrivacyPolicyScreen onAccept={handleAccept} />;
   }
 
   return (
